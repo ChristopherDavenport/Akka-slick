ca2494495fcd7054502013e9abcb67d644c37676
Christopher Davenport <ChristopherDavenport@outlook.com>
2016-03-14 11:41:22 -0400

    Updated Confifuration to use Environmental Variables

--- src/main/scala/utils/ConfigurationModule.scala
+++ src/main/scala/utils/ConfigurationModule.scala
@@ -10,7 +10,7 @@ trait Configuration {
 
 trait ConfigurationModuleImpl extends Configuration {
   private val internalConfig: Config = {
-    val configDefaults = ConfigFactory.load(this.getClass().getClassLoader(), "application.conf")
+    val configDefaults = ConfigFactory.load(this.getClass.getClassLoader, "application.conf")
     
     scala.sys.props.get("application.config") match {
       case Some(filename) => ConfigFactory.parseFile(new File(filename)).withFallback(configDefaults)
